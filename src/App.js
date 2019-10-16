fb4ad48f33151e39c24d217222d9035bee023b46
George <george.emad@outlook.com>
2019-10-16 18:57:33 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import actions : mapStateToProps & mapDispatchToProps
+import {mapStateToProps , mapDispatachToProps} from "./actions/action";
+
+//connect: used to connect our App with the global store created
+import { connect } from "react-redux"
+
+class App extends React.Component {
+
+
+  render(){
+    return (
+      <div className="app">
+
+          {/* click to increase or decrese using Redux */}
+          <div className="topDisplay">
+            <h2>display age:  {this.props.age}</h2>
+            <br/>
+            <button onClick={this.props.increase}>increase</button> &nbsp;&nbsp;&nbsp;
+            <button onClick={this.props.decrease}>decrease</button>  
+          </div>
+          
+          {/* Display age number on click & onclick on each one removed */}
+          <div className="displayData">
+            <h3>add value of increase/decrease + onClick on anyone it deleted</h3>
+            <ul className="ageList">
+              {
+                this.props.ageArray.map( (element) => {
+                  return (
+                    /* the click event should be here to target each element being clicked */
+                    <li key={element.id} onClick={()=>this.props.delete(element.id)}>
+                      {element.age}
+                    </li>
+                  )
+                } )
+              }
+            </ul>
+          </div>
+
+          {/* increase the number asynchronously */}
+          <div className="async">
+             <h3><span style={{fontWeight:"normal"}}>Thunk async Number update:</span> <strong style={{fontSize:"1.2em"}}>{this.props.asyncNumber}</strong></h3>
+             <button onClick={this.props.asyncUpdate}>update after 2 seconds</button>
+          </div>
+
+      </div>
+    )
+  }
+
 }
 
-export default App;
+//use connect to create higher order component, take comp. do something & return a new component
+export default connect(mapStateToProps,mapDispatachToProps)(App);
--- src/index.js
+++ src/index.js
@@ -4,9 +4,26 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducer from "./reducers/reducer";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+//import Provider
+import { Provider }  from "react-redux"
+
+//import Thunk
+import thunk from "redux-thunk";
+
+//import store
+import  { createStore , applyMiddleware } from "redux";
+
+
+
+//create store
+let store = createStore(reducer , applyMiddleware(thunk))
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>
+, document.getElementById('root'));
+
 serviceWorker.unregister();
